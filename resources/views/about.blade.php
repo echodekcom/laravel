760268cf502dbf47348cc72d939782f1f55180bf
Ibnu Bastamam <muhamafaiz.b@gmail.com>
2017-07-25 21:58:32 +0700

    Controllers, Routes,  Views | Laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>เกี่ยวกับ</h1>
+    <p>
+      echo "DEKCOM";
+    </p>
+    <h3>Name : {{$n}} <small>( {{$nc}} )</small></h3>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('about','SiteController@index');
 
 Route::get('/', function () {
     return view('welcome');
