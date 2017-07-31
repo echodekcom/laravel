8ef24b173445ae7eaf690e7030a80cdc2329122a
Ibnu Bastamam <muhamafaiz.b@gmail.com>
2017-07-31 09:15:28 +0700

    Web Forms | Laravel

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -26,6 +26,7 @@ class BooksController extends Controller
     public function create()
     {
         //
+          return view('books.create');
     }
 
     /**
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,6 +5,7 @@
 <div class="container">
   <div class="row">
     <div class="col-md-10 col-md-offset-1">
+      <?= link_to('books/create', $title = 'เพิ่มข้อมูล', ['class' => 'btn btn-primary'], $secure = null); ?>
       <hr>
       <div class="panel panel-default">
         <div class="panel-heading">
